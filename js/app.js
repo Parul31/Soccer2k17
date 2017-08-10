6ad8850dd48b7950ec1b1b3b0c211b657847d041
Parul Gupta <parul.uit@gmail.com>
2017-08-11 00:32:03 +0530

    final commit

--- js/app.js
+++ js/app.js
@@ -1,155 +1,254 @@
-var dates = new Array();
-var venues = new Array();
-function map() {
-	mapboxgl.accessToken = 'pk.eyJ1IjoicGFydWwzMSIsImEiOiJjajYxemtxcDAweXVuMnFuejlwMXp5OXJ2In0.Ur-gLsJ8I1IgDQGNhSWRsg';
-	var map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/mapbox/satellite-streets-v10'
-	});
-// 	map.on('load',function() {
-// 		function mapOnloadCall(dates) {
-// 	mapOnload(dates);
-// }
-// 	})
-	
-}
-// map();
-// function mapOnload(dates) {
-	
-// 		document.getElementById('slider').addEventListener('input', function(e) {
-  	
-//             var this_date = parseInt(e.target.value, 10);
-//             console.log(dates[this_date].date);
-//             filterBy(dates[this_date].date);
+var initialData = [
+	{
+		date:"2017-08-06",
+		id:"sr:match:12148638",
+		time:"15:00",
+		venue : {
+			"name": "Grolsch Veste Stadium",
+	        "capacity": 30205,
+	        "city_name": "Enschede",
+	        "country_name": "Netherlands",
+	        "lat": "52.236667",
+	        "lng":"6.837500",
+	        "country_code": "NLD"
+	    },
+	    "competitors": [
+            {
+                "id": "sr:competitor:32484",
+                "name": "Netherlands",
+                "country": "Netherlands",
+                "country_code": "NLD",
+                "abbreviation": "NED",
+                "flag": "img/netherlands.svg",
+                "qualifier": "home"
+            },
+            {
+                "id": "sr:competitor:7414",
+                "name": "Denmark",
+                "country": "Denmark",
+                "country_code": "DNK",
+                "abbreviation": "DEN",
+                "flag": "img/denmark.svg",
+                "qualifier": "away"
+            }
+        ],
+        "winner_id": "sr:competitor:32484"
+	},
+	{
+		date:"2017-08-03",
+		id:"sr:match:12107474",
+		time:"18:45",
+		venue: {
+			"name":"Grolsch Veste Stadium",
+			"capacity":30205,
+			"city_name":"Enschede",
+			"country_name":"Netherlands",
+			"lat": "52.236667",
+	        "lng":"6.837500",
+			"country_code":"NLD"	
+		},
+		"competitors": [
+            {
+                "id": "sr:competitor:32484",
+                "name": "Netherlands",
+                "country": "Netherlands",
+                "country_code": "NLD",
+                "abbreviation": "NED",
+                "flag": "img/netherlands.svg",
+                "qualifier": "home"
+            },
+            {
+                "id": "sr:competitor:7415",
+                "name": "England",
+                "country": "England",
+                "country_code": "ENG",
+                "abbreviation": "ENG",
+                "flag": "img/england.png",
+                "qualifier": "away"
+            }
+        ],
+        "winner_id": "sr:competitor:32484"
+	},
+	{
+		date:"2017-08-03",
+		id:"sr:match:12107406",
+		time:"16:00",
+		venue: {
+			"name":"Rat Verlegh Stadion",
+			"capacity":19000,
+			"city_name":"Breda",
+			"country_name":"Netherlands",
+			"lat": "51.595556",
+	        "lng":"4.750278",
+			"country_code":"NLD"
+		},
+		"competitors": [
+            {
+                "id": "sr:competitor:7414",
+                "name": "Denmark",
+                "country": "Denmark",
+                "country_code": "DNK",
+                "abbreviation": "DEN",
+                "flag": "img/denmark.svg",
+                "qualifier": "home"
+            },
+            {
+                "id": "sr:competitor:34014",
+                "name": "Austria",
+                "country": "Austria",
+                "country_code": "AUT",
+                "abbreviation": "AUT",
+                "flag": "img/austria.svg",
+                "qualifier": "away"
+            }
+        ],
+        "winner_id": "sr:competitor:7414"
+	},
+	{
+		date:"2017-07-30",
+		id:"sr:match:12095326",
+		time:"18:45",
+		venue: {
+			"name":"De Adelaarshorst",
+			"capacity":8000,
+			"city_name":"Deventer",
+			"country_name":"Netherlands",
+			"lat": "52.260416",
+	        "lng":"6.172578",
+			"country_code":"NLD"
+		},
+		"competitors": [
+            {
+                "id": "sr:competitor:7415",
+                "name": "England",
+                "country": "England",
+                "country_code": "ENG",
+                "abbreviation": "ENG",
+                "flag": "img/england.png",
+                "qualifier": "home"
+            },
+            {
+                "id": "sr:competitor:32485",
+                "name": "France",
+                "country": "France",
+                "country_code": "FRA",
+                "abbreviation": "FRA",
+                "flag": "img/france.svg",
+                "qualifier": "away"
+            }
+        ],
+        "winner_id": "sr:competitor:7415"
+	},
+	{
+		date:"2017-07-30",
+		id:"sr:match:12095324",
+		time:"16:00",
+		venue: {
+			"name":"Willem II Stadion",
+			"capacity":14637,
+			"city_name":"Tilburg",
+			"country_name":"Netherlands",
+			"lat": "51.542778",
+	        "lng":"5.066944",
+			"country_code":"NLD"
+		},
+		"competitors": [
+            {
+                "id": "sr:competitor:34014",
+                "name": "Austria",
+                "country": "Austria",
+                "country_code": "AUT",
+                "abbreviation": "AUT",
+                "flag": "img/austria.svg",
+                "qualifier": "home"
+            },
+            {
+                "id": "sr:competitor:37371",
+                "name": "Spain",
+                "country": "Spain",
+                "country_code": "ESP",
+                "abbreviation": "ESP",
+                "flag": "img/spain.svg",
+                "qualifier": "away"
+            }
+        ],
+        "winner_id": "sr:competitor:34014"
+	}
 
-//         });
-// }
-function initialDate(dates) {
-	document.getElementById('date').textContent = dates[0].date;
+];
+var map;
+var marker;
+var popup ;
+function initMap() {
+	L.mapbox.accessToken = 'pk.eyJ1IjoicGFydWwzMSIsImEiOiJjajYxemtxcDAweXVuMnFuejlwMXp5OXJ2In0.Ur-gLsJ8I1IgDQGNhSWRsg';
+	map = L.mapbox.map('map')
+		.setView([52.236667,6.837500],17);
+	popup = L.popup()
+        .setLatLng([52.236667,6.837500])
+        .setContent("<h3>Grolsch Veste Stadium</h3>")
+        .openOn(map);
+	// Use styleLayer to add a Mapbox style created in Mapbox Studio
+	L.mapbox.styleLayer('mapbox://styles/parul31/cj66ev2y377av2sn4uy2t6pk1').addTo(map);
 }
-function filterBy(dates) {
-    initialDate(dates);
+initMap();
+function popUp(lat,lng,sportsdata) {
+map.on('click', function(e) {
+	var this_lat=Math.floor(lat*10000)/10000;
+	var this_lng=Math.floor(lng*10000)/10000;
+    var latitude = Math.floor(e.latlng.lat*10000)/10000;
+    var longitude = Math.floor(e.latlng.lng*10000)/10000;
+    console.log(this_lat + " - " + this_lng);
+    console.log(latitude + " - " + longitude);
+    if((this_lat==latitude) && (this_lng==longitude)) {
+		console.log('insideif');
+		popup = L.popup()
+        .setLatLng([this_lat,this_lng])
+        .setContent("<h3>"+sportsdata.venue.name+"</h3>")
+        .openOn(map);
+	}
+  });
+}
+function displayInfo(currentSportsData) {
+	var heading = currentSportsData.date+" @"+currentSportsData.time;
+	var desc = currentSportsData.venue.name;
+	$('#display-heading').text("The event took place on "+heading);
+	$('#display-desc').text("The event took place in "+currentSportsData.venue.country_name+" in the city "+currentSportsData.venue.city_name+" at "+desc);
+	$('#country1').attr('src',currentSportsData.competitors[0].flag);
+	$('#country2').attr('src',currentSportsData.competitors[1].flag);
+	$('#country1-name').text(currentSportsData.competitors[0].name);
+	$('#country2-name').text(currentSportsData.competitors[1].name);
+	currentSportsData.competitors.forEach(function(competitor) {
+		if(competitor.id == currentSportsData.winner_id) {
+			$('#winner').text(" "+competitor.name+"("+competitor.country_code+")");
+		}
+	});
+}
+function initialDate(data) {
+	document.getElementById('date').textContent = data[0].date;
+	displayInfo(data[0]);
+}
+function filterBy(sportsdata) {
+    initialDate(sportsdata);
     document.getElementById('slider').addEventListener('input', function(e) {
   	
-            var this_date = parseInt(e.target.value, 10);
-            console.log(dates[this_date].date);
-            // filterBy(dates[this_date].date);
-            document.getElementById('date').textContent = dates[this_date].date;
-            
-        });
-}
-function getMoreData(eventId) {
-	var request = 'https://api.sportradar.us/soccer-t3/intl/en/matches/'+eventId+'/summary.json?api_key=4k6cumz6ek9a5zehvqen7v66';
-	
-	fetch(request,{method: 'GET'}).then(function(response) {
-		console.log('I am here' + response);
-		return response.json();
-
-	}).then(function(moreData) {
-		console.log(moreData);
-		var latlong=moreData.sport_event_conditions.venue.map_coordinates.split(',');
-		venues.push({
-			name: moreData.sport_event_conditions.venue.name,
-			city: moreData.sport_event_conditions.venue.city_name,
-			country: moreData.sport_event_conditions.venue.country_name,
-			lat: latlong[0],
-			lng: latlong[1]
-		});
-		
-		// console.log(venue);
-		return ;
-	});
-}
-
-
-
-function fetchData() {
-	fetch('https://api.sportradar.us/soccer-t3/intl/en/tournaments/sr:tournament:477/results.json?api_key=4k6cumz6ek9a5zehvqen7v66',{method:'GET'}).then(function(response) {
-  return response.json();
-}).then(function(mydata) {
-	var results = mydata.results;
-	// var obj = {};
-  // console.log(results);
-  
-  results.forEach(function(result) {
-  	// this.obj = {
-  	// 	id: result.sport_event.id,
-  	// 	date: result.sport_event.scheduled.slice(0,10),
-  	// 	time: result.sport_event.scheduled.slice(11,16)
-  	// }
-  	// 
-  	dates.push({
-  		id: result.sport_event.id,
-  		date: result.sport_event.scheduled.slice(0,10),
-  		time: result.sport_event.scheduled.slice(11,16)
-  	});
-  })
-  // console.log(dates);
- 	
-  // getMoreData(dates[0].id);
-  // filterBy(dates);
-  // mapOnloadCall(dates);
-  return dates;
-}).then(function(mydates) {
-	// console.log(mydates);
-	return Promise.all (
- 		mydates.map(function(date) {
- 			console.log(date.id);
- 			getMoreData(date.id)
- 		})
- 		)
-}).then(function(venues) {
-	console.log(venues);
-});
-}
-// fetchData();
-data = [
-	{
-		header: "221b Baker St.",
-		para: "November 1895. London is shrouded in fog and Sherlock Holmes and Watson pass time restlessly awaiting a new case. \"The London criminal is certainly a dull fellow,\" Sherlock bemoans. \"There have been numerous petty thefts,\" Watson offers in response. Just then a telegram arrives from Sherlock's brother Mycroft with a mysterious case."
-	},
-	{
-		header: "Aldgate Station",
-		para: "Arthur Cadogan West was found dead, head crushed in on train tracks at Aldgate Station at 6AM Tuesday morning. West worked at Woolwich Arsenal on the Bruce-Partington submarine, a secret military project. Plans for the submarine had been stolen and seven of the ten missing papers were found in West's possession. Mycroft implores Sherlock to take the case and recover the three missing papers."
-	},
-	{
-		header: "London Bridge",
-		para: "Holmes and Watson's investigations take them across London. Sherlock deduces that West was murdered elsewhere, then moved to Aldgate Station to create the illusion that he was crushed on the tracks by a train. On their way to Woolwich Sherlock dispatches a telegram to Mycroft at London Bridge: \"Send list of all foreign spies known to be in England, with full address.\""
-	}
-];
-
-var sections = function(data) {
-	this.heading = data.header;
-	this.desc = data.para;
-}
-var sportsEvent = function(data) {
-	this.id = data.sport_event.id;
-  	this.date = data.sport_event.scheduled.slice(0,10);
-  	this.time = data.sport_event.scheduled.slice(11,16);
+        var this_date = parseInt(e.target.value, 10);
+        var this_id = sportsdata[this_date].id;
+        console.log(this_id);
+        document.getElementById('date').textContent = sportsdata[this_date].date;
+        console.log(sportsdata[this_date].venue);
+        map.setView([sportsdata[this_date].venue.lat,sportsdata[this_date].venue.lng],17);
+        popUp(sportsdata[this_date].venue.lat,sportsdata[this_date].venue.lng,sportsdata[this_date]);
+		displayInfo(sportsdata[this_date]);
+    });
 }
 var viewModel = function() {
-	map();
-	fetchData();
-	
-	// $.getJSON("https://api.sportradar.us/soccer-t3/intl/en/tournaments/sr:tournament:477/results.json?api_key=4k6cumz6ek9a5zehvqen7v66", function(data) { 
- //    // Now use this data to update your view models, 
- //    // and Knockout will update your UI automatically 
- //    // console.log(data.results);
- //    self.dates = ko.observableArray([]);
- //    data.results.forEach(function(result) {
- //    	self.dates.push(new sportsEvent(result));
- //    	getMoreData(result.sport_event.id);
- //    })
-    	
- //    console.log(self.dates());
- //    console.log(self.venues());
-	// })
-	
 	var self = this;
-	this.display = ko.observableArray([]);
-	data.forEach(function(info) {
-		self.display.push(new sections(info));
-	});
-	console.log(self.display());
-}
+	self.venues = ko.observableArray([]);
+	self.soccerData = ko.observableArray([]);
+	initialData.forEach(function(data) {
+    	self.soccerData.push(data);
+    });
+    console.log(self.soccerData());
+    filterBy(self.soccerData());
+};
 ko.applyBindings(new viewModel());
