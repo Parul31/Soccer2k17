a3ad217ff0015939683e2abd19b55cf9f1a0096e
Parul Gupta <parul.uit@gmail.com>
2017-08-10 10:02:27 +0530

    map fly

--- js/app.js
+++ js/app.js
@@ -1,108 +1,115 @@
-var dates = new Array();
-var venues = new Array();
-function map() {
+var initialData = [
+	{
+		date:"2017-08-06",
+		id:"sr:match:12148638",
+		time:"15:00",
+		venue : {
+			"name": "Grolsch Veste Stadium",
+	        "capacity": 30205,
+	        "city_name": "Enschede",
+	        "country_name": "Netherlands",
+	        "lat": "52.236667",
+	        "lng":"6.837500",
+	        "country_code": "NLD"
+	    }
+	},
+	{
+		date:"2017-08-03",
+		id:"sr:match:12107474",
+		time:"18:45",
+		venue: {
+			"name":"Grolsch Veste Stadium",
+			"capacity":30205,
+			"city_name":"Enschede",
+			"country_name":"Netherlands",
+			"lat": "52.236667",
+	        "lng":"6.837500",
+			"country_code":"NLD"	
+		}
+	},
+	{
+		date:"2017-08-03",
+		id:"sr:match:12107406",
+		time:"16:00",
+		venue: {
+			"name":"Rat Verlegh Stadion",
+			"capacity":19000,
+			"city_name":"Breda",
+			"country_name":"Netherlands",
+			"lat": "51.595556",
+	        "lng":"4.750278",
+			"country_code":"NLD"
+		}
+	},
+	{
+		date:"2017-07-30",
+		id:"sr:match:12095326",
+		time:"18:45",
+		venue: {
+			"name":"De Adelaarshorst",
+			"capacity":8000,
+			"city_name":"Deventer",
+			"country_name":"Netherlands",
+			"lat": "52.260416",
+	        "lng":"6.172578",
+			"country_code":"NLD"
+		}
+	},
+	{
+		date:"2017-07-30",
+		id:"sr:match:12095324",
+		time:"16:00",
+		venue: {
+			"name":"Willem II Stadion",
+			"capacity":14637,
+			"city_name":"Tilburg",
+			"country_name":"Netherlands",
+			"lat": "51.542778",
+	        "lng":"5.066944",
+			"country_code":"NLD"
+		}
+	}
+
+];
+
+var map;
+function initMap() {
 	mapboxgl.accessToken = 'pk.eyJ1IjoicGFydWwzMSIsImEiOiJjajYxemtxcDAweXVuMnFuejlwMXp5OXJ2In0.Ur-gLsJ8I1IgDQGNhSWRsg';
-	var map = new mapboxgl.Map({
+	map = new mapboxgl.Map({
 	container: 'map',
+	center:[6.837500,52.236667],
+	zoom:16,
 	style: 'mapbox://styles/mapbox/satellite-streets-v10'
 	});
-// 	map.on('load',function() {
-// 		function mapOnloadCall(dates) {
-// 	mapOnload(dates);
-// }
-// 	})
-	
-}
-// map();
-// function mapOnload(dates) {
-	
-// 		document.getElementById('slider').addEventListener('input', function(e) {
-  	
-//             var this_date = parseInt(e.target.value, 10);
-//             console.log(dates[this_date].date);
-//             filterBy(dates[this_date].date);
 
-//         });
-// }
-function initialDate(dates) {
-	document.getElementById('date').textContent = dates[0].date;
 }
-function filterBy(dates) {
-    initialDate(dates);
+initMap();
+function changeLngLat(lng,lat) {
+
+}
+
+function initialDate(data) {
+	document.getElementById('date').textContent = data[0].date;
+}
+function filterBy(sportsdata) {
+    initialDate(sportsdata);
     document.getElementById('slider').addEventListener('input', function(e) {
   	
             var this_date = parseInt(e.target.value, 10);
-            console.log(dates[this_date].date);
+            var this_id = sportsdata[this_date].id;
+            console.log(this_id);
             // filterBy(dates[this_date].date);
-            document.getElementById('date').textContent = dates[this_date].date;
+            document.getElementById('date').textContent = sportsdata[this_date].date;
+            console.log(sportsdata[this_date].venue);
+
+            map.flyTo({
+        		center: [sportsdata[this_date].venue.lng,sportsdata[this_date].venue.lat],
+        		zoom: 16
+        	});
             
         });
 }
-function getMoreData(eventId) {
-	var request = 'https://api.sportradar.us/soccer-t3/intl/en/matches/'+eventId+'/summary.json?api_key=4k6cumz6ek9a5zehvqen7v66';
-	
-	fetch(request,{method: 'GET'}).then(function(response) {
-		console.log('I am here' + response);
-		return response.json();
 
-	}).then(function(moreData) {
-		console.log(moreData);
-		var latlong=moreData.sport_event_conditions.venue.map_coordinates.split(',');
-		venues.push({
-			name: moreData.sport_event_conditions.venue.name,
-			city: moreData.sport_event_conditions.venue.city_name,
-			country: moreData.sport_event_conditions.venue.country_name,
-			lat: latlong[0],
-			lng: latlong[1]
-		});
-		
-		// console.log(venue);
-		return ;
-	});
-}
-
-
-
-function fetchData() {
-	fetch('https://api.sportradar.us/soccer-t3/intl/en/tournaments/sr:tournament:477/results.json?api_key=4k6cumz6ek9a5zehvqen7v66',{method:'GET'}).then(function(response) {
-  return response.json();
-}).then(function(mydata) {
-	var results = mydata.results;
-	// var obj = {};
-  // console.log(results);
-  
-  results.forEach(function(result) {
-  	// this.obj = {
-  	// 	id: result.sport_event.id,
-  	// 	date: result.sport_event.scheduled.slice(0,10),
-  	// 	time: result.sport_event.scheduled.slice(11,16)
-  	// }
-  	// 
-  	dates.push({
-  		id: result.sport_event.id,
-  		date: result.sport_event.scheduled.slice(0,10),
-  		time: result.sport_event.scheduled.slice(11,16)
-  	});
-  })
-  // console.log(dates);
- 	
-  // getMoreData(dates[0].id);
-  // filterBy(dates);
-  // mapOnloadCall(dates);
-  return dates;
-}).then(function(mydates) {
-	// console.log(mydates);
-	return Promise.all (
- 		mydates.map(function(date) {
- 			console.log(date.id);
- 			getMoreData(date.id)
- 		})
- 		)
-}).then(function(venues) {
-	console.log(venues);
-});
-}
-// fetchData();
 data = [
 	{
 		header: "221b Baker St.",
@@ -122,30 +129,20 @@ var sections = function(data) {
 	this.heading = data.header;
 	this.desc = data.para;
 }
-var sportsEvent = function(data) {
-	this.id = data.sport_event.id;
-  	this.date = data.sport_event.scheduled.slice(0,10);
-  	this.time = data.sport_event.scheduled.slice(11,16);
-}
+
 var viewModel = function() {
-	map();
-	fetchData();
-	
-	// $.getJSON("https://api.sportradar.us/soccer-t3/intl/en/tournaments/sr:tournament:477/results.json?api_key=4k6cumz6ek9a5zehvqen7v66", function(data) { 
- //    // Now use this data to update your view models, 
- //    // and Knockout will update your UI automatically 
- //    // console.log(data.results);
- //    self.dates = ko.observableArray([]);
- //    data.results.forEach(function(result) {
- //    	self.dates.push(new sportsEvent(result));
- //    	getMoreData(result.sport_event.id);
- //    })
-    	
- //    console.log(self.dates());
- //    console.log(self.venues());
-	// })
-	
 	var self = this;
+
+	self.venues = ko.observableArray([]);
+	self.soccerData = ko.observableArray([]);
+	initialData.forEach(function(data) {
+    	self.soccerData.push(data);
+    })
+    console.log(self.soccerData());
+
+    filterBy(self.soccerData());
+
+	
 	this.display = ko.observableArray([]);
 	data.forEach(function(info) {
 		self.display.push(new sections(info));
